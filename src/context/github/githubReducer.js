739571ed518ba64c6fabc31469bcc079db1b77b6
Simone Celia <simon387@hotmail.it>
2019-12-09 11:56:34 +0100

    upd

--- src/context/github/githubReducer.js
+++ src/context/github/githubReducer.js
@@ -1,6 +1,27 @@
+import {
+	SEARCH_USERS,
+	SET_LOADING,
+	CLEAR_USER,
+	GET_REPOS
+} from '../types';
 
-const GithubReducer = () => {
 
+const GithubReducer = (state, action) => {
+	switch(action.type) {
+		case SEARCH_USERS:
+			return {
+				...state,
+				users: action.payload,
+				loading: false
+			}
+		case SET_LOADING:
+			return {
+				...state,
+				loading: true
+			}
+		default:
+			return state;
+	}
 }
 
 export default GithubReducer;
