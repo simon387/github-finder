e6cf1c2f4af32ba7b3d18716d5e99addce6cdfbe
Simone Celia <simon387@hotmail.it>
2019-12-09 11:56:34 +0100

    upd

--- src/App.js
+++ src/App.js
@@ -18,14 +18,6 @@ const App = () => {
 	const [loading, setLoading] = useState(false);
 	const [alert, setAlert] = useState(null);
 	const [repos, setRepos] = useState([]);
-	
-	//search github users
-	const searchUsers = async (text) => {
-		setLoading(true);
-		const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
-		setUsers(res.data.items);
-		setLoading(false);
-	}
 
 	//get single github user
 	const getUser = async (username) => {
@@ -64,7 +56,7 @@ const App = () => {
 						<Route exact path='/' render={props => (
 							<Fragment>
 								<Search 
-									searchUsers={searchUsers} clearUsers={clearUsers}
+									clearUsers={clearUsers}
 									showClear={ users.length > 0 ? true : false}
 									setAlert={showAlert}/>
 								<Users loading={loading} users={users} />
